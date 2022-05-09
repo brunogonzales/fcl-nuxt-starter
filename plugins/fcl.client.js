b02272cf8383618c5ddb2c4f8b29995ea1c0a055
Bruno Gonzales <57231813+brunogonzales@users.noreply.github.com>
2022-05-09 10:29:40 -0500

    update to fcl alpha

--- plugins/fcl.client.js
+++ plugins/fcl.client.js
@@ -1,8 +1,10 @@
 import * as fcl from "@onflow/fcl";
 
 fcl.config({
-  "accessNode.api": "https://access-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
-  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest.onflow.org"
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn",
+  "app.detail.title": "Kitty Items", // Shows user what dapp is trying to connect
+  "app.detail.icon": "https://fcl-discovery.onflow.org/images/blocto.png", // shows image to the user to display your dapp brand
   "0xProfile": "0xba1132bc08f82fe2" // The account address where the smart contract lives
 });
 
